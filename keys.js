2e554f67d32cf3d14ec5b7225aa1a670ae7a38ff
Pentester Helper <dev.krrathod@gmail.com>
2025-07-18 09:53:58 +0530

    1.2

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // keys.js
 module.exports = {
-  GEMINI_API_KEY: 'AIzaSyD64BiPKGTqiJPwL5qg7BJjH9gaclg1_pM',
-  VIRUSTOTAL_API_KEY: 'cda45d623904430e23c428a17beddb22ffea5d84fcbb60f52711745780729b50'
+  GEMINI_API_KEY: '',
+  VIRUSTOTAL_API_KEY: ''
 };
